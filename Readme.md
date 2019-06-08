11bdd64625167a31a63564a42c55877d36e3470e
Ilia Motornyi <ilia.motornyi@jetbrains.com>
2019-06-08 10:41:58 +0300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,37 @@
+A small project to run QEMU in ARM Cortex-M3 mode with remote gdb endpoint.
+=====
+
+Primarily for testing CLion embedded support.
+
+[CLion](https://jetbrains.com/clion/) 2019.2+ is required with embedded support plugin enabled.
+
+Features
+---
+Uses QEMU emulation of TI lm3s811evb.QEMU is run with some garbage (README file) instead of kernel image. The binary is uploaded via gdb anyway but qemu
+requires some kernel to start.
+
+QEMU cmd arguments:
+* `-cpu cortex-m3` - cpu type
+* `-M lm3s811evb` - guest machine type
+* `-nographic` - cpu type
+* `-s` - cli shortcut to start gdbserver at port 1234
+* `-S` - freeze execution, wait until gdb is connected
+* `-kernel README` - load binary garbage as kernel
+
+Standard CMSIS 5 for Cortex-M3 + libnano are used. CMSIS is modified according to lm3s memory map.
+
+
+`stdout` is redirected to lm3s UART0.
+
+
+
+There is a [fake peripheral device definition](fake_peripheral.svd) which may be used for debugging and testing
+purposes. The "device" is located in RAM, at address `0x20001800`. In the program code it is filled up with value.
+
+
+Clion Run configuration
+---
+
+![Clion Run configuration](runconfig.png)
+
+Still work in progress
---
+++ main.c
@@ -0,0 +1,59 @@
+#include <stdint-gcc.h>
+#include <stdio.h>
+
+typedef struct {
+    uint8_t REG_8;
+    uint16_t REG_16;
+    uint32_t REG_32;
+    union {
+        uint32_t REG_32BITFIELD;
+        struct {
+            unsigned int field1:1;
+            unsigned int field2:2;
+            unsigned int field3:3;
+            unsigned int field4:4;
+            unsigned int :4;
+            unsigned int field5:5;
+            unsigned int field6:6;
+            unsigned int field7:7;
+        };
+    };
+} __attribute__((packed)) FAKE_PERIPHERAL_TYPE;
+
+FAKE_PERIPHERAL_TYPE *FAKE_PERIPHERAL = (FAKE_PERIPHERAL_TYPE *)0x20001800;
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+
+int main(void) {
+    printf("Size of the peripheral struct %d\n", sizeof(*FAKE_PERIPHERAL));
+
+    FAKE_PERIPHERAL->REG_32BITFIELD = 0xABCDEF79;
+    FAKE_PERIPHERAL->REG_8 = 0xCC;
+    FAKE_PERIPHERAL->REG_16 = 0xBCBC;
+    FAKE_PERIPHERAL->REG_32 = 0xAA55C396;
+    FAKE_PERIPHERAL->field5 = 9;
+
+    for (int i = 0; 1; i++) {
+        printf("Hello, World!!! %x\n", i);
+        for (long  j = 0; j < 100000000L; j++) {
+            __asm__("nop");
+        }
+    }
+}
+
+#pragma clang diagnostic pop
+
+
+// Redirect file handlers to UART0
+// TI LM3S-specific address is used
+// Nor readiness check, nor configuration - this stuff works fine under qemu
+
+#define UART0_DR *((uint32_t *) 0x4000c000)
+
+__attribute__((unused)) int _write(__attribute__((unused))int file, const char *data, int len) {
+    for (int i = 0; i < len; i++) {
+        UART0_DR = data[i];
+    }
+    return len;
+}
+
